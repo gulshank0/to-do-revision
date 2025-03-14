df10bbe55e5dd9acdfc09d2775099d7edcd8e922
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-14 23:01:58 +0530

    mongoodb done

--- db.js
+++ db.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+mongoose.connect(mongodb+srv://gulshan63072:Uh6SwMQA2oxswTj2@kirat.i9fgd.mongodb.net/todos);
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description:String,
+  completed: Boolean,
+});
+
+
+const todo =mongoose.model('todo',todoSchema);
+
+module.exports={
+    todo:todo
+}
