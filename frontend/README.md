d4b0d48ef9e5b119dab9191306cba6e91635d1af
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-15 02:19:40 +0530

    folder destructured and ugly fronted added

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+## it is a ToDo App
+
+
---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {   
+    return (
+        <div>
+<input type="text" placeholder="title" />
+<br />
+<input type="text" placeholder="description" />     
+<br />
+<button>Add todo</button>
+
+
+        </div>
+    )
+}
