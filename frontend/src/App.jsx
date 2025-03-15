fe68b2fef4e7a5a2d17ef9807d60a1948ef826f6
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-16 00:07:33 +0530

    complete work done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
+import React, { useState } from 'react'
 import './App.css'
 import Card from './components/Card.jsx'
+import Todos from './components/Todos.jsx'
 
 function App() {
+const [todos, setTodos] = useState([]);
+
+fetch("http://localhost:3000/todos")
+.then(async function(res){
+  const json = await res.json();
+  setTodos(json.todos);
+})
+
+
 
   return (
     <>
    
    <Card/> 
-     
+    <Todos todos={todos}/> 
     </>
   )
 }
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,12 +1,36 @@
-export default function Card() {   
+import React, { useState } from 'react'
+
+export default function Card() {  
+    const [title, setTitle] = useState(""); 
+    const [description, setDescription] = useState("");
     return (
         <div className="flex flex-col h-screen align-center items-center  bg-black " >
             <p className="text-white text-3xl py-2 " >Add Todo for today </p>
-<input className="bg-white text-black text-center rounded-xl" type="text" placeholder="title" />
+<input className="bg-white text-black text-center rounded-xl" type="text" placeholder="title" onChange={function(e){
+    const value = e.target.value;
+    setTitle(e.target.value)
+} } />
 <br />
-<input className="bg-white text-black text-center rounded-xl " type="text" placeholder="description" />     
+<input className="bg-white text-black text-center rounded-xl " type="text" placeholder="description" onChange={function(e){
+    const value = e.target.value;
+    setTitle(e.target.value)
+} } />     
 <br />
-<button className="bg-teal-300 px-10 rounded-xl " >Add todo</button>
+<button className="bg-teal-300 px-10 rounded-xl hover:cursor-pointer hover:bg-teal-900 " onClick={()=>{
+    fetch("http://localhost:3000/todo", {
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title,
+            description
+        })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}} >Add todo</button>
 
 
         </div>
