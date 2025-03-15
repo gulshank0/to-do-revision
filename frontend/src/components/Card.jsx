30b6fa87b410df1f19ac72637de8a523ec2a0bac
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-15 21:59:32 +0530

    added tailwind for style

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,11 +1,12 @@
 export default function Card() {   
     return (
-        <div>
-<input type="text" placeholder="title" />
+        <div className="flex flex-col h-screen align-center items-center  bg-black " >
+            <p className="text-white text-3xl py-2 " >Add Todo for today </p>
+<input className="bg-white text-black text-center rounded-xl" type="text" placeholder="title" />
 <br />
-<input type="text" placeholder="description" />     
+<input className="bg-white text-black text-center rounded-xl " type="text" placeholder="description" />     
 <br />
-<button>Add todo</button>
+<button className="bg-teal-300 px-10 rounded-xl " >Add todo</button>
 
 
         </div>
